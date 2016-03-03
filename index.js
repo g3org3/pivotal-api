cb05625302baaa3dd918ac6beaeb6adc9571f02b
g3org3 <7jagjag@gmail.com>
2016-03-03 10:04:28 -0600

    add more calls, get accounts, get memberships, get projects

--- index.js
+++ index.js
@@ -14,76 +14,124 @@ const Internals = {};
  */
 function PivotalAPI (_token) {
 
-	if( !(this instanceof PivotalAPI) ) return new PivotalAPI(_token)
-	if (typeof _token == 'undefined') throw new Error('No token provided');
+   if( !(this instanceof PivotalAPI) ) return new PivotalAPI(_token)
+   if (typeof _token == 'undefined') throw new Error('No token provided');
 
 
-	Internals.baseUrl = 'https://www.pivotaltracker.com/services/v5'
-	Internals.options = {
-		headers: {
-			'X-TrackerToken': _token
-		}
-		// open_timeout: 20000
-	}
+   Internals.baseUrl = 'https://www.pivotaltracker.com/services/v5'
+   Internals.options = {
+      headers: {
+         'X-TrackerToken': _token
+      }
+      // open_timeout: 20000
+   }
 
-	/*
-	 * Get My Info
-	 * - Gets pivotal tracker personal info of the token provided
-	 */
-	this.getMyinfo = (callback) => {
+   /*
+    * Get My Info
+    * - Gets pivotal tracker personal info of the token provided
+    */
+   this.getMyinfo = (callback) => {
+      return Internals.apiCall('/me', callback)
+   }
 
-		if (typeof callback == 'function')
-         return Internals.apiCall('/me', callback)
-      return Internals.apiCallQ('/me')
- 	}
+   /*
+    * Get Accounts
+    */
+   this.getAccounts = (callback) => {
+      return Internals.apiCall('/accounts', callback)
+   }
+
+   /*
+    * Get Projects
+    */
+   this.getProjects = (callback) => {
+      return Internals.apiCall('/projects', callback)
+   }
+
+   /*
+    *
+    */
+   this.getAccountMembership = (id, callback) => {
+      return Internals.apiCall(`/accounts/${id}/memberships`, callback)
+   }
+
+   /*
+    * Get Account Memberships
+    */
+   this.getAccountsMemberships = (accounts) => {
+
+      var getAccounts = []
+      for (var i = 0; i < accounts.length; i++) {
+         getAccounts.push(this.getAccountMembership(accounts[i].id))
+      }
+      return Q.all(getAccounts)
+   }
+
+   /*
+    * General
+    */
+   this.call = (url, callback) => {
+      return Internals.apiCall(url, callback)
+   }
 }
 
+/*
+ *
+ */
+Internals.apiCall = (url, callback) => {
+   if (typeof callback == 'function') {
+      return Internals._apiCall(url, callback)
+   }
+   else {
+      return Internals._apiCallQ(url)
+   }
+}
 
 /*
  * Api Call Main Method
  */
-Internals.apiCall = (url, callback) => {
+Internals._apiCall = (url, callback) => {
 
-	Needle.get(Internals.baseUrl+url, Internals.options, function(err, resp) {
+   Needle.get(Internals.baseUrl+url, Internals.options, function(err, resp) {
 
-		if (err) {
-			return callback(err)
-		}
-		if (resp.statusCode != 200) {
-			resp.body.statusCode = resp.statusCode
-			var error = new Error(JSON.stringify(resp.body, null, 4))
-			error.statusCode = resp.statusCode
-			return callback(error)
-		}
-		return callback(null, resp.body)
-	});
+      if (err) {
+         return callback(err)
+      }
+      if (resp.statusCode != 200) {
+         resp.body.statusCode = resp.statusCode
+         var error = new Error(JSON.stringify(resp.body, null, 4))
+         error.statusCode = resp.statusCode
+         return callback(error)
+      }
+      return callback(null, resp.body)
+   });
 }
 
 /*
  * Api Call with promise
  */
-Internals.apiCallQ = (url, callback) => {
+Internals._apiCallQ = (url, callback) => {
 
-	var NeedleGetQ = Q.denodeify(Needle.get)
-	var defer = Q.defer()
+   var NeedleGetQ = Q.denodeify(Needle.get)
+   var defer = Q.defer()
 
-	NeedleGetQ(Internals.baseUrl+url, Internals.options)
-	.then(results => {
-		if (results[0].statusCode != 200) {
-			results[0].body.statusCode = results[0].statusCode
-			var error = new Error(JSON.stringify(results[0].body, null, 4))
-			error.statusCode = results[0].statusCode
-			defer.reject(error);
-		}
-		else {
-			defer.resolve(results[0].body);
-		}
+   NeedleGetQ(Internals.baseUrl+url, Internals.options)
+   .then(results => {
+      if (results[0].statusCode != 200) {
+         results[0].body.statusCode = results[0].statusCode
+         var error = new Error(JSON.stringify(results[0].body, null, 4))
+         error.statusCode = results[0].statusCode
+         defer.reject(error);
+      }
+      else {
+         defer.resolve(results[0].body);
+      }
 
-	})
-	.fail(err => {
-		defer.reject(err)
-	})
-	return defer.promise;
+   })
+   .fail(err => {
+      defer.reject(err)
+   })
+   return defer.promise;
 }
 
 module.exports = PivotalAPI;
--- test.js
+++ test.js
@@ -1,16 +1,74 @@
 
 const PivotalAPi = require('./index')
-const API = PivotalAPi(process.env.PIVOTAL_TOKEN)
+const Api = PivotalAPi(process.env.PIVOTAL_TOKEN)
+const Purdy = require('purdy')
+const defaultErr = err => console.error(err)
+const _ = require('lodash')
+const Internals = {}
 
-API.getMyinfo(function (err, res) {
-	if (err) {
-		console.log('Error> ', err.message)
-	}
-	else {
-		console.log(res.name)
-	}
-})
+Internals.array2Object = (array, key) => {
+	return _.reduce(array, (_u, obj) => {
+		_u[obj[key]] = obj;
+		return _u;
+	}, {})
+}
 
-API.getMyinfo()
-.then(res => console.log(res.name))
-.fail(err => console.log(err))
+Internals.getProjects = () => {
+	Api.getMyinfo()
+	.then(me => {
+		// Purdy(me)
+		Internals.myId = me.id
+		return Api.getAccounts()
+	})
+	.then(accounts => {
+
+		// Purdy(_.map(accounts, (account) => {
+		// 	return { id: account.id, name: account.name }
+		// }))
+
+		Internals.accounts = _.map(accounts, (account) => {
+			return { id: account.id, name: account.name }
+		})
+
+		return Api.getAccountsMemberships(accounts)
+	})
+	.then(response => {
+
+		for (var i = 0; i < response.length; i++) {
+
+			response[i] = _.reduce(response[i], (_response, res) => {
+				if (res.id == Internals.myId) {
+					_response = {
+						id: res.id,
+						person: JSON.stringify(res.person),
+						owner: res.owner,
+						admin: res.admin,
+						project_creator: res.project_creator
+					}
+				}
+				return _response
+			}, {})
+
+
+			response[i].name = Internals.accounts[i].name
+			response[i].accountId = Internals.accounts[i].id
+		}
+		response = Internals.array2Object(response, 'accountId')
+		Internals.memberships = response;
+		// Purdy(response)
+		return Api.call('/projects')
+	})
+	.then(projects => {
+
+		var validProjects = []
+		for (var i = 0; i < projects.length; i++) {
+			var membership = Internals.memberships[projects[i].account_id]
+			if (membership && (membership.owner || membership.admin) ) {
+				validProjects.push(projects[i])
+			}
+		}
+
+		Purdy(validProjects)
+	})
+	.fail(defaultErr)
+}
